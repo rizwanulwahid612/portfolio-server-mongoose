2ff53574de25cdf4a5c4a05495078460fe7622f6
rizwanulwahid612 <rizwanulwahid612@gmail.com>
2024-01-25 20:55:26 +0600

    check

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ let server: Server;
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url);
     console.log(`🛢   Database is connected successfully`);
 
     server = app.listen(config.port, () => {
